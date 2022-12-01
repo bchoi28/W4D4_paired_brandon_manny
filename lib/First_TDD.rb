afa32604307094708f6667beff68eee880b4bf25
Brandon Choi <bchoi28@gmail.com>
2022-12-01 15:29:10 -0500

    added two_sum

--- lib/First_TDD.rb
+++ lib/First_TDD.rb
@@ -6,6 +6,26 @@ class Array
             end
         hash.keys
     end
+
+    def two_sum
+        pairs = []
+        
+        self.each_with_index do |ele1, i|
+            self.each_with_index do |ele2, j|
+                if j > i && ele1 + ele2 == 0
+                    pairs << [i,j]
+                end
+            end
+        end
+        pairs
+    end
 end
 
 
+def my_transpose(arrays)
+    transposed = []
+    
+    (0...arrays.length-1)
+
+    transposed
+end
--- spec/First_TDD_spec.rb
+++ spec/First_TDD_spec.rb
@@ -5,3 +5,15 @@ describe "Array#my_uniq" do
         expect([1,2,1,3,3].my_uniq).to eq([1,2,3])
     end
 end
+
+describe "Array#two_sum" do
+    it "find all pairs of positions where the elements at those positions sum to zero" do
+        expect([-1, 0, 2, -2, 1].two_sum).to eq([[0,4], [2,3]])
+    end
+end
+
+describe "#my_transpose" do
+    it "converts between the row oriented and column oriented representations" do
+        expect(my_transpose([[0, 1, 2],[3, 4, 5],[6, 7, 8]])).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+    end
+end
